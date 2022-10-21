0803882b59e122c10c146037c2ae4ef2617eea20
spencercoleman <spencer@edc.vc>
2022-10-20 18:33:08 -0700

    Define RootState and implement TypedSelector to handle selecting state from PackagesList

--- src/components/PackagesList.tsx
+++ src/components/PackagesList.tsx
@@ -1,7 +1,11 @@
 import { useState } from 'react';
+import { useTypedSelector } from '../hooks/useTypedSelector';
 import { useActions } from '../hooks/useActions';
 
 const PackagesList = (): JSX.Element => {
+    const { data, error, isLoading } = useTypedSelector(
+        (state) => state.npmPackages
+    );
     const [term, setTerm] = useState('');
     const { searchPackages } = useActions();
 
@@ -16,6 +20,18 @@ const PackagesList = (): JSX.Element => {
                 <input value={term} onChange={(e) => setTerm(e.target.value)} />
                 <button>Search</button>
             </form>
+
+            {error && <strong>{error}</strong>}
+
+            {isLoading && <strong>Loading...</strong>}
+
+            {!error && !isLoading && (
+                <ul>
+                    {data.map((name, index) => (
+                        <li key={index}>{name}</li>
+                    ))}
+                </ul>
+            )}
         </div>
     );
 };
