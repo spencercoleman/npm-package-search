6b9dc4c1be4853cb1f40b04e001c23a310998ce3
spencercoleman <spencer@edc.vc>
2022-10-22 00:41:09 -0700

    Add mantine and implement global styles

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/App';
+import { MantineProvider } from '@mantine/core';
 
 const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 
 root.render(
-    <React.StrictMode>
+    <MantineProvider
+        theme={{
+            fontFamily: 'monospace',
+
+            globalStyles: (theme) => ({
+                '*, *::before, *::after': {
+                    boxSizing: 'border-box',
+                },
+
+                body: {
+                    backgroundImage: 'linear-gradient(#c12127, #e65156)',
+                    margin: 0,
+                },
+
+                '#root': {
+                    height: '100vh',
+                },
+
+                '.mantine-Input-input': {
+                    border: 'none',
+                },
+            }),
+        }}
+    >
         <App />
-    </React.StrictMode>
+    </MantineProvider>
 );
